7aba9d82772bd42c1d5207e22f41ce72b4bf8531
TheBeaver420 <mohammediklasu@gmail.com>
2025-09-27 00:27:37 +0100

    Cleaned up code and started working on title screen to game start screen

---
+++ buttons.py
@@ -0,0 +1,5 @@
+def title_change(label,start_btn) -> None:
+    label.destroy()
+    start_btn.destroy()
+
+
--- main.py
+++ main.py
@@ -2,14 +2,40 @@ import requests
 import pyarabic.araby as araby
 from customtkinter import *
 from PIL import Image, ImageTk
+import buttons
+from buttons import title_change
+
+# app setup
 
 app = CTk()
 app.geometry("1920x1080")
 app.title("SarfTrainer")
 set_appearance_mode("dark")
 
+# label
 
-btn = CTkButton(master=app,text="Click Me", corner_radius=32,command=app.destroy,width=500,height=500,font=("Arial",200,"bold"))
-btn.place(relx=0.5, rely=0.5,anchor="center")
+label = CTkLabel(
+    app,
+    text="Sarf Trainer",
+    fg_color="transparent",
+    font=("Arabic Typesetting",100,"bold")
+)
+label.place(relx=.5,rely=.1, anchor="center")
+
+#start button
+
+start_btn = CTkButton(
+    master=app,
+    text="Start",
+    corner_radius=32,
+    command=lambda: title_change(label,start_btn),
+    width=100,
+    height=100,
+    fg_color="transparent",
+    font=("Arial",40,"bold")
+)
+start_btn.place(relx=0.5, rely=0.5,anchor="center")
+
+#run the app
 
 app.mainloop()
