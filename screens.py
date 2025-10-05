186858290cdb146f642d70fb23e04e17fb4e0a6f
TheBeaver420 <mohammediklasu@gmail.com>
2025-10-05 09:48:14 +0100

    Added quiz screen and other functions

--- screens.py
+++ screens.py
@@ -4,12 +4,14 @@ from widgets import create_title, create_button
 class SarfTrainerApp:
     def __init__(self,root):
         self.root = root
+
+        # Widgets
         self.title = None
         self.start_btn = None
         self.next_label = None
-        self.quiz_label = None
+        self.quiz_frame = None
 
-        # get monitor resolution
+        # Get monitor resolution
 
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
@@ -19,7 +21,7 @@ class SarfTrainerApp:
         self.root.geometry(f"{screen_width}x{screen_height}+0+0")
         self.root.resizable(True,True)
 
-        #Ui Setup
+        # Ui Setup
 
         self.root.title("Sarf Trainer")
         set_appearance_mode("dark")
@@ -27,7 +29,12 @@ class SarfTrainerApp:
         # Start screen
         self.show_start_screen()
 
+    def clear_screen(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
     def show_start_screen(self):
+        self.clear_screen()
         # Create title
         self.title = create_title(self.root,"Sarf Trainer")
         self.title.place(relx=0.5, rely=0.1, anchor="center")
@@ -38,9 +45,7 @@ class SarfTrainerApp:
         self.start_btn.place(relx=0.5, rely=0.2, anchor="center")
 
     def show_options_screen(self):
-        # Destroy old widgets
-        self.title.destroy()
-        self.start_btn.destroy()
+        self.clear_screen()
 
         # Show next screen
 
@@ -51,7 +56,47 @@ class SarfTrainerApp:
         self.start_btn = create_button(self.root,"Done!",command=self.show_quiz_screen)
         self.start_btn.place(relx=0.5, rely=0.3, anchor="center")
 
+
+
     def show_quiz_screen(self):
-        self.next_label.destroy()
-        self.quiz_label = create_title(self.root,"Question 1:")
-        self.quiz_label.place(relx=0.5, rely=0.1, anchor="center")
+        self.clear_screen()
+        self.quiz_frame = QuizFrame(
+            master = self.root,
+            question_text="What does the word كتب mean?" ,
+            on_submit=self.process_answer
+        )
+        self.quiz_frame.pack(fill="both",expand=True)
+
+    def process_answer(self,answer):
+        print(f"User selected:{answer}")
+        self.show_quiz_screen()
+
+#Defines frame that shows a question, checkboxes and a submit button
+class QuizFrame(CTkFrame):
+    def __init__(self,master, question_text, on_submit):
+        super().__init__(master)
+        self.onsubmit = on_submit
+
+        # Create question text
+        self.question_label = create_title(self, question_text)
+        self.question_label.pack(pady=30)
+
+        # Answer checkboxes
+        self.answers_vars = []
+        for i in range(4):
+            var = IntVar()
+            checkbox = CTkCheckBox(
+                self, text=f"Option {i+1}", variable=var
+            )
+            checkbox.pack(pady=30)
+            self.answers_vars.append(checkbox)
+
+        #submit button
+        submit_btn = create_button(self, "Submit", command=self.submit)
+        submit_btn.pack(pady=30)
+
+    def submit(self):
+        selected_answer = [i + 1 for i, var in enumerate(self.answers_vars) if var.get() == 1]
+        print("Selected answers:", selected_answer)
+        self.onsubmit(selected_answer)
+
