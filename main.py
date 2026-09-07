43b55a25baea78c961e3fb3d1666854e57f95e4a
unknown <Iklas>
2026-09-07 22:00:24 +0100

    Integrate Camel Tools for Arabic morphology

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-
 from customtkinter import *
-
-
 from screens import SarfTrainerApp
 from widgets import create_title, create_button
 
 
-# # app setup
+# app setup
 # def main():
 #     app = CTk()
 #     SarfTrainerApp(app)
 #     app.mainloop()
-#
+
 # if __name__ == "__main__":
-#     # main()
+#     main()
